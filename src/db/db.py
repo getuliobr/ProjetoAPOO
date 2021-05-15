559e9aef5c73d9aa390a607709fd45ac536a0bff
Nuisigor <dexchanel@gmail.com>
2021-05-15 20:41:06 -0300

    Mudanças na Database

--- src/db/db.py
+++ src/db/db.py
@@ -2,8 +2,8 @@ from os.path import isfile
 from sqlite3 import connect
 from apscheduler.triggers.cron import CronTrigger
 
-DB_PATH = "./database.db"
-BUILD_PATH = "./build.sql"
+DB_PATH = "../../data/db/database.db"
+BUILD_PATH = "../../data/db/build.sql"
 
 cxn = connect(DB_PATH, check_same_thread=False)
 cur = cxn.cursor()
@@ -25,10 +25,6 @@ def build():
 
 def commit():
 	cxn.commit()
-
-
-def autosave(sched):
-	sched.add_job(commit, CronTrigger(second=0))
 
 
 def close():
